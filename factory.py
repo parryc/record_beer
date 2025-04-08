ad162a3474538873dcbc5ef9eeea42897aeedc75
parry <parry@parryc.com>
2025-04-08 18:53:35 -0500

    Add pool options to sqlalchemy

--- factory.py
+++ factory.py
@@ -5,5 +5,7 @@ from flask import Flask
 def create_app(name=__name__):
     app = Flask(name)
     app.config.from_object("config.DevelopmentConfig")
+    # https://help.pythonanywhere.com/pages/UsingSQLAlchemywithMySQL#configuring-sqlalchemy
+    app.config["SQLALCHEMY_ENGINE_OPTIONS"] = {"pool_recycle": 280}
     db.init_app(app)
     return app
